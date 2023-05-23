681fcb24d5744f1133afbcda287d3c5bd459a683
Abdrakhmanov Mukhamedkarim <j3t1x0h@gmail.com>
2023-05-23 21:26:49 +0600

    123

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -4,6 +4,10 @@ import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
+        for (String str : args) {
+            if (!StringUtils.isPositiveNumber(str)) {
+                return false;
+            }
+        }
         return true;
     }
-}
