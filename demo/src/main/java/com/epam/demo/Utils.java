07bc634decc7889d15072e5353c8e14f82e1d80f
Abdrakhmanov Mukhamedkarim <j3t1x0h@gmail.com>
2023-05-24 21:36:28 +0600

    start

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -4,10 +4,6 @@ import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        for (String str : args) {
-            if (!StringUtils.isPositiveNumber(str)) {
-                return false;
-            }
-        }
-        return true;
+        //magic happens here
     }
+}
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,7 +2,6 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        return org.apache.commons.lang3.StringUtils.isNumeric(str) && Double.parseDouble(str) > 0;
+         //here magic will happen
     }
 }
-
