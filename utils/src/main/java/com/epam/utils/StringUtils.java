7e2e083ae6329131f87d7c446e861b390934a5e9
Abdrakhmanov Mukhamedkarim <j3t1x0h@gmail.com>
2023-05-24 23:10:49 +0600

    over

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,11 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-         //here magic will happen
+        try {
+            int number = Integer.parseInt(str);
+            return number > 0;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
